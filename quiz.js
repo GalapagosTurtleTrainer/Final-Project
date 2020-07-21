f9a28595d00d8c16e9476bed420154d1012ed475
amazingkenigsberg <62404757+amazingkenigsberg@users.noreply.github.com>
2020-07-21 11:57:37 -0400

    psuedocode js logic

---
+++ quiz.js
@@ -0,0 +1,95 @@
+pizzaChoice = 0
+if (click.vegan) {
+    //take me to quiestion 2a
+    pizzaChoice + 1
+}
+if (click.meat) {
+    //take me to quiestion 2b
+    pizzaChoice + 2
+}
+if (click.pepperoni) {
+    //take me to quiestion 3
+    pizzaChoice + 2
+}
+if (click.sausage) {
+    //take me to quiestion 3
+    pizzaChoice + 3
+}
+if (click.mushroom) {
+    //take me to quiestion 3
+    pizzaChoice + 1
+}
+if (click.broccoli) {
+    //take me to quiestion 3
+    pizzaChoice + 2
+}
+if (click.thin) {
+    //take me to quiestion 4
+    pizzaChoice + 2
+}
+if (click.deepDish) {
+    //take me to quiestion 4
+    pizzaChoice + 6
+}
+if (click.fruits) {
+    //take me to error screen
+}
+if (click.noFruits) {
+    //take me to question 5
+}
+if (click.classic) {
+    pizzaChoice + 4
+}
+if (click.expiriment) {
+    pizzaChoice + 12
+}
+if (pizzaChoice = 8){
+    //take me to mushroomClassic
+}
+if (pizzaChoice = 9){
+    //take me to vedgetableClassic
+}
+if (pizzaChoice = 12){
+    //take me to mushroomChicagoStyle
+}
+if (pizzaChoice = 13){
+    //take me to vedgetableChicagoStyle
+}
+if (pizzaChoice = 16){
+    //take me to turkishPizza
+}
+if (pizzaChoice = 17){
+    //take me to stuffedCrustCauliflower
+}
+if (pizzaChoice = 20){
+    //take me to chicagoStyleMushroomPeppers
+}
+if (pizzaChoice = 21){
+    //take me to chicagoStyleGrandmaPie
+}
+if (pizzaChoice = 10){
+    //take me to pepperoniPizza
+}
+if (pizzaChoice = 11){
+    //take me to sausagePizza
+}
+if (pizzaChoice = 14){
+    //take me to chicagoDeepDishPepperoni
+}
+if (pizzaChoice = 15){
+    //take me to chicagoDeepDishSausage
+}
+if (pizzaChoice = 18){
+    //take me to pepperoniCheeseStuffedCrust
+}
+if (pizzaChoice = 19){
+    //take me to carneAsada
+}
+if (pizzaChoice = 22){
+    //take me to deepDishPepperoniStuffedCrust
+}
+if (pizzaChoice = 23){
+    //take me to deepDishSausageStuffedCrust
+}
+
+
