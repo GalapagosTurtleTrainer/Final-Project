0edbf3d7642eb93500643b5a2452069b050b9c10
amazingkenigsberg <62404757+amazingkenigsberg@users.noreply.github.com>
2020-07-22 10:47:32 -0400

    JS is done all htmland ids are up to date

--- quiz.js
+++ quiz.js
@@ -1,95 +1,99 @@
 var pizzaChoice = 0
-if (onclick.vegan) {
-    location.href = "question2a.html"
+if (onclick.perfect-pizza) {
+    location.href = "Question1.html"
+    pizzaChoice + 1
+}
+if (onclick.Vegetable_button) {
+    location.href = "Question2a.html"
     pizzaChoice + 1
 }
 if (onclick.Meat_button) {
-    location.href = "question2b.html"
+    location.href = "Question2b.html"
     pizzaChoice + 2
 }
-if (onclick.pepperoni) {
-    location.href = "question3.html"
+if (onclick.Pepperoni_button) {
+    location.href = "Question3.html"
     pizzaChoice + 2
 }
-if (onclick.sausage) {
-    location.href = "question3.html"
+if (onclick.Sausage_button) {
+    location.href = "Question3.html"
     pizzaChoice + 3
 }
-if (onclick.mushroom) {
-    location.href = "question3.html"
+if (onclick.Mushroom_button) {
+    location.href = "Question3.html"
     pizzaChoice + 1
 }
-if (onclick.broccoli) {
-    location.href = "question3.html"
+if (onclick.Broccoli_button) {
+    location.href = "Question3.html"
     pizzaChoice + 2
 }
-if (onclick.thin) {
-    location.href = "question4.html"
+if (onclick.ThinPizza_button) {
+    location.href = "Question4.html"
     pizzaChoice + 2
 }
-if (onclick.deepDish) {
-    location.href = "question4.html"
+if (onclick.DeepDish_button) {
+    location.href = "Question4.html"
     pizzaChoice + 6
 }
-if (onclick.fruits) {
+if (onclick.Illegal_button) {
     location.href = "404.html"
 }
-if (onclick.noFruits) {
-    location.href = "question5.html"
+if (onclick.LawAbidingCitizen_button) {
+    location.href = "Question5.html"
 }
-if (onclick.classic) {
+if (onclick.Classic_button) {
     pizzaChoice + 4
 }
-if (onclick.expiriment) {
+if (onclick.Experimental_button) {
     pizzaChoice + 12
 }
 if (pizzaChoice = 8){
-    location.href = "question3.html"
+    location.href = "mushroomclassic.html"
 }
 if (pizzaChoice = 9){
-    location.href = "question3.html"
+    location.href = "veggiepizza.html"
 }
 if (pizzaChoice = 12){
-    location.href = "question3.html"
+    location.href = "mushroomdeep.html"
 }
 if (pizzaChoice = 13){
-    location.href = "question3.html"
+    location.href = "veggiedeep.html"
 }
 if (pizzaChoice = 16){
-    location.href = "question3.html"
+    location.href = "mushroomexp.html"
 }
 if (pizzaChoice = 17){
-    location.href = "question3.html"
+    location.href = "veggieexp.html"
 }
 if (pizzaChoice = 20){
-    location.href = "question3.html"
+    location.href = "mushroompepperdeep.html"
 }
 if (pizzaChoice = 21){
-    location.href = "question3.html"
+    location.href = "veggiedeepexp.html"
 }
 if (pizzaChoice = 10){
-    location.href = "question3.html"
+    location.href = "pepperoni.html"
 }
 if (pizzaChoice = 11){
-    location.href = "question3.html"
+    location.href = "sausage.html"
 }
 if (pizzaChoice = 14){
-    location.href = "question3.html"
+    location.href = "pepperonideep.html"
 }
 if (pizzaChoice = 15){
-    location.href = "question3.html"
+    location.href = "sausagedeep.html"
 }
 if (pizzaChoice = 18){
-    location.href = "question3.html"
+    location.href = "pepperoniexp.html"
 }
 if (pizzaChoice = 19){
-    location.href = "question3.html"
+    location.href = "sausageexp.html"
 }
 if (pizzaChoice = 22){
-    location.href = "question3.html"
+    location.href = "pepperonideepexp.html"
 }
 if (pizzaChoice = 23){
-    location.href = "question3.html"
+    location.href = "sausagedeepexp.html"
 }
 
 
