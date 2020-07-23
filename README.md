df3a19d36e4493c3b390e21317497cac5c33acf2
amazingkenigsberg <62404757+amazingkenigsberg@users.noreply.github.com>
2020-07-23 10:27:14 -0400

    fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Final-Project
+1. Get pages running
+2. Build js logic
+    a. add to a variable
+    b. navigate pages
+    c. serve results
+3. Add bootstrap functions and css
--- quiz.js
+++ quiz.js
@@ -1,99 +1,120 @@
-var pizzaChoice = 0
-if (onclick.perfect-pizza) {
-    location.href = "Question1.html"
-    pizzaChoice + 1
-}
-if (onclick.Vegetable_button) {
-    location.href = "Question2a.html"
-    pizzaChoice + 1
-}
-if (onclick.Meat_button) {
-    location.href = "Question2b.html"
-    pizzaChoice + 2
-}
-if (onclick.Pepperoni_button) {
-    location.href = "Question3.html"
-    pizzaChoice + 2
-}
-if (onclick.Sausage_button) {
-    location.href = "Question3.html"
-    pizzaChoice + 3
-}
-if (onclick.Mushroom_button) {
-    location.href = "Question3.html"
-    pizzaChoice + 1
-}
-if (onclick.Broccoli_button) {
-    location.href = "Question3.html"
-    pizzaChoice + 2
-}
-if (onclick.ThinPizza_button) {
-    location.href = "Question4.html"
-    pizzaChoice + 2
-}
-if (onclick.DeepDish_button) {
-    location.href = "Question4.html"
-    pizzaChoice + 6
-}
-if (onclick.Illegal_button) {
-    location.href = "404.html"
-}
-if (onclick.LawAbidingCitizen_button) {
-    location.href = "Question5.html"
-}
-if (onclick.Classic_button) {
-    pizzaChoice + 4
-}
-if (onclick.Experimental_button) {
-    pizzaChoice + 12
-}
-if (pizzaChoice = 8){
-    location.href = "mushroomclassic.html"
-}
-if (pizzaChoice = 9){
-    location.href = "veggiepizza.html"
-}
-if (pizzaChoice = 12){
-    location.href = "mushroomdeep.html"
-}
-if (pizzaChoice = 13){
-    location.href = "veggiedeep.html"
-}
-if (pizzaChoice = 16){
-    location.href = "mushroomexp.html"
-}
-if (pizzaChoice = 17){
-    location.href = "veggieexp.html"
-}
-if (pizzaChoice = 20){
-    location.href = "mushroompepperdeep.html"
-}
-if (pizzaChoice = 21){
-    location.href = "veggiedeepexp.html"
-}
-if (pizzaChoice = 10){
-    location.href = "pepperoni.html"
-}
-if (pizzaChoice = 11){
-    location.href = "sausage.html"
-}
-if (pizzaChoice = 14){
-    location.href = "pepperonideep.html"
-}
-if (pizzaChoice = 15){
-    location.href = "sausagedeep.html"
-}
-if (pizzaChoice = 18){
-    location.href = "pepperoniexp.html"
-}
-if (pizzaChoice = 19){
-    location.href = "sausageexp.html"
-}
-if (pizzaChoice = 22){
-    location.href = "pepperonideepexp.html"
-}
-if (pizzaChoice = 23){
-    location.href = "sausagedeepexp.html"
+var pizzaChoice
+
+// local storage vs session storage https://www.google.com/search?q=session+storage+vs+local+storage&oq=session+storage+vs+local+storage&aqs=chrome..69i57j0l7.6760j0j4&sourceid=chrome&ie=UTF-8
+if (sessionStorage.getItem("pizzaChoice")) {
+    pizzaChoice = parseInt(sessionStorage.getItem("pizzaChoice"))
+} else {
+    pizzaChoice = 0
+    sessionStorage.setItem("pizzaChoice", 0)
 }
 
+function startFunction() {
+    location.replace("Question1.html")
+}
+function veggieFunction() {
+    pizzaChoice = pizzaChoice + 1
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question2b.html")
+}
+function meatFunction() {
+    pizzaChoice = pizzaChoice + 2
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question2a.html")
+}
+function pepperoniFunction() {
+    pizzaChoice = pizzaChoice + 2
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question3.html")
+}
+function sausageFunction() {
+    pizzaChoice = pizzaChoice + 3
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question3.html")
+}
+function mushroomFunction() {
+    pizzaChoice = pizzaChoice + 1
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question3.html")
+}
+function broccoliFunction() {
+    pizzaChoice = pizzaChoice + 2
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question3.html")
+}
+function thinFunction() {
+    pizzaChoice = pizzaChoice + 2
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question4.html")
+}
+function deepFunction() {
+    pizzaChoice = pizzaChoice + 6
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    location.replace("Question4.html")
+}
+function fruitsFunction() {
+    location.replace("404.html")
+}
+function noFruitsFunction() {
+    location.replace("Question5.html")
+}
+function classicFunction() {
+    pizzaChoice = pizzaChoice + 4
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    result();
+}
+function expFunction() {
+    pizzaChoice = pizzaChoice + 12
+    sessionStorage.setItem("pizzaChoice", pizzaChoice)
+    result();
+}
+function result() {
+    if (pizzaChoice === 8) {
+        location.replace("mushroomclassic.html")
+    }
+    if (pizzaChoice === 9) {
+        location.replace("veggiepizza.html")
+    }
+    if (pizzaChoice === 12) {
+        location.replace("mushroomdeep.html")
+    }
+    if (pizzaChoice === 13) {
+        location.replace("veggiedeep.html")
+    }
+    if (pizzaChoice === 16) {
+        location.replace("mushroomexp.html")
+    }
+    if (pizzaChoice === 17) {
+        location.replace("veggieexp.html")
+    }
+    if (pizzaChoice === 20) {
+        location.replace("mushroompepperdeep.html")
+    }
+    if (pizzaChoice === 21) {
+        location.replace("veggiedeepexp.html")
+    }
+    if (pizzaChoice === 10) {
+        location.replace("pepperoni.html")
+    }
+    if (pizzaChoice === 11) {
+        location.replace("sausage.html")
+    }
+    if (pizzaChoice === 14) {
+        location.replace("pepperonideep.html")
+    }
+    if (pizzaChoice === 15) {
+        location.replace("sausagedeep.html")
+    }
+    if (pizzaChoice === 18) {
+        location.replace("pepperoniexp.html")
+    }
+    if (pizzaChoice === 19) {
+        location.replace("sausageexp.html")
+    }
+    if (pizzaChoice === 22) {
+        location.replace("pepperonideepexp.html")
+    }
+    if (pizzaChoice === 23) {
+        location.replace("sausagedeepexp.html")
+    }
+}
 
